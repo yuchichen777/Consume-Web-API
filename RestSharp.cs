c3c72ad91864aa2a05ceaf35bdf1b0f85c2441e7
yuchichen777 <63004350+yuchichen777@users.noreply.github.com>
2020-05-20 13:39:04 +0800

    Update RestSharp.cs

--- RestSharp.cs
+++ RestSharp.cs
@@ -1,19 +1,85 @@
+using Newtonsoft.Json;
 using RestSharp;
 using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp
 {
-    class Program
+    public class RestSharpApiService : IApiService
     {
-        static void Main(string[] args)
+        private readonly string baseUrl = "http://localhost:8000/api";
+
+        //x-www-form-urlencoded
+        public ResponseModel<T> Post()
         {
-            var client = new RestClient("https://localhost:8000/api/Post");
+            var apiUrl = baseUrl + "/Post";
+            var client = new RestClient(apiUrl);
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
             request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
             request.AddParameter("Key", "Value");
             IRestResponse response = client.Execute(request);
-            Console.WriteLine(response.Content);
+
+            var jsonString = response.Content;
+
+            return JsonConvert.DeserializeObject<ResponseModel<T>>(jsonString);
+        }
+
+        public ResponseModel<T> Get(string token)
+        {
+            var date = DateTime.Now.ToString("yyyy/MM/dd");
+            var apiUrl = baseUrl + $"/Get?QueryDate={date}";
+
+            var bearer = "Bearer " + token;
+
+            var client = new RestClient(apiUrl);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", bearer);
+            IRestResponse response = client.Execute(request);
+            var jsonString = response.Content;
+
+            return JsonConvert.DeserializeObject<ResponseModel<T>>(jsonString);
+        }
+
+        public ResponseModel<T> Get(string token)
+        {
+            var apiUrl = baseUrl + "/Get";
+
+            var bearer = "Bearer " + token;
+
+            var client = new RestClient(apiUrl);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", bearer);
+            IRestResponse response = client.Execute(request);
+            var jsonString = response.Content;
+
+            return JsonConvert.DeserializeObject<ResponseModel<T>>(jsonString);
+        }
+
+        public ResponseModel<T> Post(string token)
+        {
+            var apiUrl = baseUrl + "/Post";
+            var bearer = "Bearer " + token;
+
+            var client = new RestClient(apiUrl);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json; charset=utf-8");
+            request.AddHeader("Authorization", bearer);
+            request.AddParameter(
+                "application/json; charset=utf-8",
+                "[\n" +
+                "    {\n" +
+                "        \"Id\": \"1\",\n" +
+                "        \"Name\": \"Name\",\n" +
+                "    }\n" +
+                "]", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            var jsonString = response.Content;
+
+            return JsonConvert.DeserializeObject<ResponseModel<T>>(jsonString);
         }
     }
 }
