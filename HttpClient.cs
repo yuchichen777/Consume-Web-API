a10ec567092bb265f8f9e981238c2602b1eee527
yuchichen777 <63004350+yuchichen777@users.noreply.github.com>
2020-05-20 14:04:30 +0800

    Update HttpClient.cs

--- HttpClient.cs
+++ HttpClient.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace ConsoleApp
 {
-    public class HttpClient
+    public class HttpClientApiService : IApiService
     {
         private readonly string baseUrl = "http://localhost/api";
 
